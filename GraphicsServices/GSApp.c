0dedbd71bb68ecb99ca56196e77608ed6f2ab5cc
Robert Widmann <devteam.codafi@gmail.com>
2015-08-01 12:52:54 -0600

    Rename GSEvent to GSApp

---
+++ GraphicsServices/GSApp.c
@@ -0,0 +1,109 @@
+//
+//  GSApp.c
+//  GraphicsServices
+//
+//  Created by Robert Widmann on 8/1/15.
+//  Copyright © 2015 CodaFi. All rights reserved.
+//
+
+#include "GSApp.h"
+#include "GSPrivate.h"
+
+void GSAppRun() {
+	GSAppRunModal(false);
+}
+
+void GSAppRegisterEventCallBack(void (^callback)(GSEventRef)) {
+	___eventCallBack = callback;
+}
+
+static CFMutableArrayRef ___runLoopModeStack;
+static bool ___exitRunModal;
+
+void GSAppPushRunLoopMode(CFStringRef mode) {
+	if (___runLoopModeStack == NULL) {
+		___runLoopModeStack = CFArrayCreateMutable(NULL, 0, &kCFTypeArrayCallBacks);
+	}
+	CFArrayAppendValue(___runLoopModeStack, mode);
+	CFRunLoopStop(___eventRunLoop);
+}
+
+void GSAppPopRunLoopMode(CFStringRef mode) {
+	if ((___runLoopModeStack == NULL) || (CFArrayGetCount(___runLoopModeStack) <= 0)) {
+		CFRunLoopStop(___eventRunLoop);
+		return;
+	}
+	
+	CFStringRef topLoopMode = CFArrayGetValueAtIndex(___runLoopModeStack, CFArrayGetCount(___runLoopModeStack) - 1);
+	if ((topLoopMode != NULL) && (CFStringCompare(mode, topLoopMode, 0) == kCFCompareEqualTo)) {
+		CFArrayRemoveValueAtIndex(___runLoopModeStack, CFArrayGetCount(___runLoopModeStack) - 1);
+	}
+	CFRunLoopStop(___eventRunLoop);
+}
+
+void GSAppRunModal(bool disallowsRestarts) {
+	while (true) {
+		if (disallowsRestarts == false) {
+			if ((___runLoopModeStack == NULL) || (CFArrayGetCount(___runLoopModeStack) <= 0)) {
+				fprintf(stderr, "%s: NULL run loop mode. Exiting loop\n", "GSAppRunModal");
+				___exitRunModal = false;
+				return;
+			}
+		}
+		
+		if (___exitRunModal) {
+			___exitRunModal = false;
+			return;
+		}
+		
+		CFStringRef topRunLoopMode = CFArrayGetValueAtIndex(___runLoopModeStack, CFArrayGetCount(___runLoopModeStack) - 1);
+		if (topRunLoopMode == NULL) {
+			fprintf(stderr, "%s: NULL run loop mode. Exiting loop\n", "GSAppRunModal");
+			___exitRunModal = false;
+			return;
+		}
+		
+		CFRunLoopRunInMode(topRunLoopMode, kCFAbsoluteTimeIntervalSince1970, disallowsRestarts);
+	}
+}
+
+void GSAppStopModal() {
+	___exitRunModal = true;
+	CFRunLoopStop(CFRunLoopGetCurrent());
+}
+
+static mach_port_name_t ___applicationPort;
+
+
+void __GSEventInitializeApp(dispatch_queue_t queue) {
+	static bool _initialized;
+	
+	if (_initialized == true) {
+		return;
+	}
+	_initialized = true;
+	
+	__GSEventInitializeShared(queue);
+	___applicationPort = CFMachPortGetPort(CGWindowServerCreateServerPort());
+	
+	if (queue != NULL) {
+		mach_port_limits_t qlimits;
+		qlimits.mpl_qlimit = 0x100;
+		mach_port_set_attributes(mach_task_self(), ___applicationPort, MACH_PORT_LIMITS_INFO, (mach_port_info_t)&qlimits, MACH_PORT_LIMITS_INFO_COUNT);
+		_GSAddSourceForEventPort(___applicationPort, queue);
+	}
+	
+	if (___eventRunLoop != NULL) {
+		_GSAddRunLoopSourceForEventPort(___applicationPort, kCFRunLoopCommonModes);
+		_GSAddRunLoopSourceForEventPort(___applicationPort, kGSEventReceiveRunLoopMode);
+	}
+	//	int err = mach_port_insert_right(mach_task_self(), ___applicationPort, ___applicationPort, MACH_MSG_TYPE_MAKE_SEND);
+	//	if (err != KERN_SUCCESS) {
+	//		fprintf(stderr, "mach_port_insert_right() error: %s\n", mach_error_string(err));
+	//		abort();
+	//	}
+}
+
+void GSAppInitialize() {
+	__GSEventInitializeApp(NULL);
+}
---
+++ GraphicsServices/GSApp.h
@@ -0,0 +1,60 @@
+//
+//  GSApp.h
+//  GraphicsServices
+//
+//  Created by Robert Widmann on 8/1/15.
+//  Copyright © 2015 CodaFi. All rights reserved.
+//
+
+#ifndef GSAPP_H
+#define GSAPP_H
+
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreGraphics/CoreGraphics.h>
+#include <Availability.h>
+#include <GraphicsServices/GSEvent.h>
+
+
+#pragma mark - Application Initialization
+
+
+/// Initializes the event pump.
+CF_EXPORT void GSAppInitialize();
+
+
+#pragma mark - Run Loops
+
+
+/// The run loop mode GraphicsServices spins the event loop in.
+CF_EXPORT const CFStringRef kGSEventReceiveRunLoopMode;
+
+/// Spins the runloop in the current mode.
+CF_EXPORT void GSAppRun();
+
+/// Pushes a new run loop mode onto the top of the stack.
+CF_EXPORT void GSAppPushRunLoopMode(CFStringRef mode);
+
+/// Pops the topmost run loop mode from the stack.
+///
+/// If there are no modes or the last mode has been popped the application will terminate.
+CF_EXPORT void GSAppPopRunLoopMode(CFStringRef mode);
+
+/// Spins the runloop in the topmost mode by initiating a modal session in the previous runloop.
+///
+/// If the `disallowsRestarts` flag is set the runloop will not automatically restart once it has
+/// stopped.
+CF_EXPORT void GSAppRunModal(bool disallowsRestarts);
+
+/// Stops the topmost runloop and transfers control to the initiator of the modal session.
+CF_EXPORT void GSAppStopModal();
+
+
+#pragma mark - Callbacks
+
+
+/// Registers a callback with the event pump.
+CF_EXPORT void GSAppRegisterEventCallBack(void (^callback)(GSEventRef));
+
+
+
+#endif /* GSAPP_H */
